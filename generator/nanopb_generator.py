0a57be722eca6a96d0932f03983db5089dccacad
openvela-robot <openvela-robot@xiaomi.com>
2019-11-09 08:40:51 +0800

    Merge pull request #451 from torfinnberset/bugfix/use-of-signed-integer-operand-in-bitwise-binary-op
    Fix clang-tidy warnings about using signed integers in binary bitwise operations

--- generator/nanopb_generator.py
+++ generator/nanopb_generator.py
@@ -1855,7 +1855,6 @@ def main_plugin():
         lex = shlex.shlex(params)
         lex.whitespace_split = True
         lex.whitespace = ','
-        lex.commenters = ''
         args = list(lex)
 
     optparser.usage = "Usage: protoc --nanopb_out=[options][,more_options]:outdir file.proto"
